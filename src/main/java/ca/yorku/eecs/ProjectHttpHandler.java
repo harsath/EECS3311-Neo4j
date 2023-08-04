a2bcba6a40345b3a71e3a06a2df96a9bae2e765a
harsath <harsath@mm.st>
2023-08-04 03:37:32 -0400

    computeBaconNumber (#7); computeBaconNumberTests (#9)

--- src/main/java/ca/yorku/eecs/ProjectHttpHandler.java
+++ src/main/java/ca/yorku/eecs/ProjectHttpHandler.java
@@ -242,8 +242,44 @@ public class ProjectHttpHandler implements HttpHandler {
   }
 
   private void computeBaconNumber(HttpExchange exchange) throws IOException {
-    String response = "computeBaconNumber";
-    sendResponse(exchange, 404, response);
+    /* Unsupported HTTP method to this API endpoint */
+    if (checkRequestMethod(exchange, "GET")) {
+      sendResponse(exchange, 405, "Endpoint only accepts GET requests.");
+      return;
+    }
+    String rawQuery = exchange.getRequestURI().getRawQuery();
+    /* Make sure useragent passes query-string */
+    if (rawQuery == null || rawQuery.isEmpty()) {
+      sendResponse(exchange, 405, "Must pass query to this endpoint.");
+      return;
+    }
+    /* Parse query string into a hashmap */
+    Map<String, String> queryParams = Utils.splitQuery(rawQuery);
+    /* Check if query property 'actorId' exists */
+    if (!queryParams.containsKey("actorId")) {
+      sendResponse(exchange, 400, "Must include 'actorId'.");
+      return;
+    }
+    String actorId = queryParams.get("actorId");
+    /* Check if 'actorId' exists in database */
+    if (!databaseExecutor.checkIfActorIdExists(actorId)) {
+      sendResponse(exchange, 404, "'actorId' does not exist on database.");
+      return;
+    }
+    JSONObject jsonObject = new JSONObject();
+    /* If `actorId` is that of Kevin Bacon, return 0 */
+    if (actorId.equals(DatabaseExecutor.kevinBaconActorId)) {
+      jsonObject.put("baconNumber", 0);
+      sendResponse(exchange, 200, jsonObject.toString());
+      return;
+    }
+    Integer baconNumber = databaseExecutor.getBaconNumber(actorId);
+    /* No path exists to the `actorId` */
+    if (baconNumber == 0) {
+      sendResponse(exchange, 404, "No path exists.");
+    }
+    jsonObject.put("baconNumber", baconNumber);
+    sendResponse(exchange, 200, jsonObject.toString());
   }
 
   private void computeBaconPath(HttpExchange exchange) throws IOException {
